04befbd712d3b0de719415eb04b52e20446a629a
PRABHAT BHARDWAJ <100183910+AWMprabhat@users.noreply.github.com>
2022-05-21 20:29:31 +0530

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+          <div>
+         
+          <h1>Mobile Opreeting System</h1>
+        
+             <li>Android</li>
+            <li>Blackbery</li>
+            <li>iPhone</li>
+            <li>Windose Phone</li>
+        
+          </div>
+           
+          
+        <div>
+        <h1>Mobile Opreeting System</h1>
+   
+<li>Android</li>
+            <li>Blackbery</li>
+            <li>iPhone</li>
+            <li>Windose Phone</li>
+        
+
+          
+           
+       
+            
+            </div>
+
+        </div>
+        
+   
+  )
+}
+
+export default Card
